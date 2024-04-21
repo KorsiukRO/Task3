81f56c048448000ba42ee1a5e1a7bf5fe5032cbd
KorsiukRO <116555976+KorsiukRO@users.noreply.github.com>
2024-04-21 13:37:05 +0300

    #1

--- Task3/FootballClub.cs
+++ Task3/FootballClub.cs
@@ -2,5 +2,24 @@ namespace Task3;
 
 public class FootballClub
 {
+    public required string Name { get; init; }
     
+    public int YearOfFoundation { get; init; }
+    
+    public required string HeadCoach { get; init; }
+
+
+    public FootballClub() { }
+
+    public FootballClub(string name, int year, string coach)
+    {
+        Name = name;
+        YearOfFoundation = year;
+        HeadCoach = coach;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}, HeadCoach: {HeadCoach}, Year of foundation: {YearOfFoundation}";
+    }
 }
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Task3;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var clubs = new Dictionary<string, List<FootballClub>>()
+        {
+            { "London", [
+                    new FootballClub() { Name = "Arsenal", YearOfFoundation = 1886, HeadCoach = "Mikel Arteta" },
+                    new FootballClub() { Name = "Chelsea", YearOfFoundation = 1905, HeadCoach = "Graham Potter" },
+                    new FootballClub() { Name = "Tottenham Hotspur", YearOfFoundation = 1882, HeadCoach = "Antonio Conte" },
+                    new FootballClub() { Name = "West Ham United", YearOfFoundation = 1895, HeadCoach = "David Moyes" },
+                    new FootballClub() { Name = "Fulham", YearOfFoundation = 1879, HeadCoach = "Marco Silva" }
+                ]
+            },
+            
+            {"Manchester", [
+                    new FootballClub()
+                        { Name = "Manchester United", YearOfFoundation = 1878, HeadCoach = "Erik ten Hag" },
+                    new FootballClub()
+                        { Name = "Manchester City", YearOfFoundation = 1880, HeadCoach = "Pep Guardiola" }
+                ]
+            },
+            
+            {"Liverpool", [
+                    new FootballClub() { Name = "Liverpool", YearOfFoundation = 1892, HeadCoach = "Jurgen Klopp" },
+                    new FootballClub() { Name = "Everton", YearOfFoundation = 1878, HeadCoach = "Sean Dyche" }
+                ]
+            },
+            
+            {"Milan", [
+                    new FootballClub() { Name = "AC Milan", YearOfFoundation = 1899, HeadCoach = "Stefano Pioli" },
+                    new FootballClub() { Name = "Inter Milan", YearOfFoundation = 1908, HeadCoach = "Simone Inzaghi" }
+                ]
+            },
+            
+            {"Madrid",
+                [new FootballClub() { Name = "Real Madrid", YearOfFoundation = 1902, HeadCoach = "Carlo Ancelotti" }]
+            },
+
+            { "Barcelona",
+                [new FootballClub() { Name = "Barcelona", YearOfFoundation = 1899, HeadCoach = "Xavi Hernandez" }]
+            },
+
+            { "Munich",
+                [new FootballClub() { Name = "Bayern Munich", YearOfFoundation = 1900, HeadCoach = "Thomas Tuchel" }]
+            },
+            
+            {"Turin",
+                [new FootballClub() { Name = "Juventus", YearOfFoundation = 1897, HeadCoach = "Massimiliano Allegri" }]
+            }
+        };
+
+        Console.Write("Input city name: ");
+        
+        var city = Console.ReadLine();
+        
+        if (city != null && clubs.TryGetValue(city, value: out var club1))
+        {
+            foreach (var club in club1)
+            {
+                Console.WriteLine(club.ToString());
+            }
+        }
+        else
+        {
+            Console.WriteLine($"No clubs found in {city}.");
+        }
+    }
+}
